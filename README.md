2083e0624e51b217b9240f8b0772b77f624459aa
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-22 18:34:04 -0700

    Added variables example.

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Scripts to teach you Python.
 For a complete beginner, it's recommended going in this order.
 
 1. comments.py - because comments are important, and will be used everywhere.
+2. variables.py - variables will be used everywhere past this point.
---
+++ variables.py
@@ -0,0 +1,28 @@
+# As in any other language, variables can have types. You don't declare them directly, though.
+
+# Strings
+myName = "Ty"
+programmingLanguage = "Python 3"
+
+# Ints
+age = 15
+
+# Floats / doubles.
+gpa = 3.79
+
+# Lists.
+# Note that lists can contain any type.
+knownLanguages = ["Python", "Java", "C#", "JavaScript"]
+
+# Dictionaries.
+testGrades = {
+	"Andy": "A-",
+	"Robby": "B+"
+}
+
+# Booleans
+isTall = False
+isBlind = True
+
+# None type (like NULL in C).
+vision = None
